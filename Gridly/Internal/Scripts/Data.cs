148314eb848ccbaa8d2e90646bd572480e07d7e1
vinpix <76024897+vinpix@users.noreply.github.com>
2021-07-09 17:30:31 +0700

    Add files via upload

---
+++ Gridly/Internal/Scripts/Data.cs
@@ -0,0 +1,137 @@
+﻿
+using UnityEngine;
+using System.IO;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Gridly.Internal
+{
+    public static class DataManager
+    {
+        public static void Save(this string key, int value)
+        {
+            PlayerPrefs.SetInt(key.Enrypt(), value);
+        }
+
+        public static void Save(this string key, float value)
+        {
+            PlayerPrefs.SetFloat(key.Enrypt(), value);
+        }
+
+        public static void Save(this string key, string value)
+        {
+            PlayerPrefs.SetString(key.Enrypt(), value);
+        }
+
+        public static void Save(this string key, bool value)
+        {
+            //key.Print(value.ToString()+" Save");
+            if (value)
+                PlayerPrefs.SetInt(key.Enrypt(), 1);
+            else PlayerPrefs.SetInt(key.Enrypt(), 0);
+        }
+
+        public static void SetIsUse(this string key, bool isUse)
+        {
+            (key + ".isUse").Save(isUse);
+        }
+
+        public static bool GetIsUse(this string key)
+        {
+            return (key + ".isUse").Load();
+        }
+
+        public static int Load(this string key, int defaultVal)
+        {
+            return PlayerPrefs.GetInt(key.Enrypt(), defaultVal);
+        }
+
+        public static float Load(this string key, float defaultVal)
+        {
+            return PlayerPrefs.GetFloat(key.Enrypt(), defaultVal);
+        }
+
+        public static string Load(this string key, string defaultVal)
+        {
+            return PlayerPrefs.GetString(key.Enrypt(), defaultVal);
+        }
+
+        public static string RemoveLast(this string key, int num)
+        {
+            return key.Remove(key.Length - num, num);
+        }
+        public static bool Load(this string key, bool defaulVal)
+        {
+            //key.Print(defaulVal.ToString() + " Load");
+            int defaultInt = 0;
+            if (defaulVal) defaultInt = 1;
+
+            return (PlayerPrefs.GetInt(key.Enrypt(), defaultInt) == 1);
+        }
+
+        public static bool Load(this string key)
+        {
+            return (PlayerPrefs.GetInt(key.Enrypt(), 0) == 1);
+        }
+
+
+        public static string CovertToRomandNumber(this int num)
+        {
+            if (num == 1)
+                return "I";
+            if (num == 2)
+                return "II";
+            if (num == 3)
+                return "III";
+            if (num == 4)
+                return "IV";
+            if (num == 5)
+                return "V";
+            return "...";
+        }
+
+
+    }
+
+    public interface MyData<T>
+    {
+
+        T getInstance();
+        void setInstance(T NewData);
+    }
+    public class Data<T> where T : MyData<T>, new()
+    {
+
+        public static void Save()
+        {
+            string dataPath = Application.persistentDataPath;
+            var format = new BinaryFormatter();
+            var stream = new FileStream(dataPath + "/" + typeof(T).Name + ".kietdeptrai", FileMode.Create);
+            //Debug.Log("saved + " + dataPath + "/" + typeof(T).Name + ".kietdeptrai");
+            format.Serialize(stream, new T().getInstance());
+            stream.Close();
+        }
+
+
+
+        public static bool Load()
+        {
+            string path = typeof(T).Name;
+            string dataPath = Application.persistentDataPath;
+            if (!System.IO.File.Exists(dataPath + "/" + path + ".kietdeptrai"))
+            {
+                //Debug.Log("cant find data in " + dataPath + "/" + path + ".kietdeptrai");
+                return false;
+            }
+
+            var ser = new BinaryFormatter();
+            var stream = new FileStream(dataPath + "/" + path + ".kietdeptrai", FileMode.Open);
+            new T().setInstance((T)ser.Deserialize(stream)); //Convert.ChangeType(ser.Deserialize(stream), typeof(T)));
+            stream.Close();
+            return true;
+        }
+
+
+    }
+   
+}
---
+++ Gridly/Internal/Scripts/GridlyFunction.cs
@@ -0,0 +1,311 @@
+using UnityEngine.Networking;
+using System;
+using UnityEngine;
+
+
+namespace Gridly.Internal
+{
+    public static class GridlyUtility
+    {
+        public static void Print(this object i, string text)
+        {
+            Debug.Log(text + " " + "<b>" + i + "</b>");
+        }
+        public static void Print(this object i)
+        {
+            string output;
+            int length = i.ToString().Length;
+
+
+            output = i.ToString();
+
+
+            if (length > 600)
+            {
+                output = i.ToString().Substring(0, 600);
+                output += "...";
+            }
+            
+            Debug.Log("<b>" + output + "</b>");
+        }
+
+        public static bool CheckOutput(this string output)
+        {
+
+            if (output.Length == 0)
+            {
+                Debug.LogError("Something is wrong. Please check your key again");
+                //EditorApplication.update = null;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class GridlyFunction
+    {
+
+        public static GridlyFunction s = new GridlyFunction();
+        static string gridlyDatatbasePath => "Assets/Gridly/Resources/Databases";
+
+
+        #region Setup
+        public void SetupDatabases()
+        {
+            RefeshDowloadTotal();
+
+            
+            foreach(var i in Project.singleton.grids)
+            {
+                i.records.Clear();
+                SetupRecords(i, 0);
+            }
+        }
+
+        public static string GetRecordPage(string viewID, int i)
+        {
+            int begin = (i * step);
+
+            Debug.Log("get record from " + (i * step) + " to: " + (i + 1) * step);
+            string _ = "https://api.gridly.com/v1/views/" + viewID + "/records?page=%7B%22offset%22%3A+" + begin.ToString() + "%2C+%22limit%22%3A+" + (step).ToString() + "%7D";
+
+            return _;
+        }
+        static int step = 1000;
+
+
+        public void SetupRecords(Grid grid, int page)
+        {
+            
+            int nprocess = 2; //download 2k record
+            for (int i = 0 + page; i < nprocess + page; i++)
+            {
+                SetupRecords(grid, i, i == (nprocess + page) - 1);
+            }
+            
+        }
+        public static int dowloadn;
+        public static int dowloadedTotal;
+        public static bool isDowloading => dowloadn < dowloadedTotal;
+        public void RefeshDowloadTotal()
+        {
+            dowloadn = 0;
+            dowloadedTotal = 0;
+        }
+
+        public void SetupRecords(Grid grid, int page, bool autoDowload)
+        {
+            if (grid == null)
+                return;
+            ("Setting up record for " + grid.nameGrid).Print();
+            dowloadedTotal += 1;
+
+            UnityWebRequest unityWeb = UnityWebRequest.Get(GetRecordPage(grid.choesenViewID, page));
+            unityWeb.SetRequestHeader("Authorization", "ApiKey " + UserData.singleton.keyAPI);
+            unityWeb.SendWebRequest();
+            
+            void action()
+            {
+                if (unityWeb.isDone)
+                {
+                    dowloadn += 1;
+                    var i = unityWeb.downloadHandler.text;
+                    if (i.CheckOutput())
+                    {
+
+                        var N = JSON.Parse(i);
+                        int index = 0;
+                        //string _path = "";
+                        
+                        while (N[index].Count != 0)
+                        {
+
+                            Record record = new Record();
+
+                            record.recordID = N[index]["id"];
+                            record.pathTag = N[index]["path"];
+                            int index1 = 0;
+                            while (N[index]["cells"][index1].Count != 0)
+                            {
+
+
+
+                                string value = "";
+
+
+                                int lengthVal = N[index]["cells"][index1]["value"].Count;
+                                for (int indexValue = 0; indexValue <= lengthVal; indexValue++)
+                                {
+                                    if (value != "")
+                                        value += ";";
+                                    if (indexValue == 0)
+                                        value += N[index]["cells"][index1]["value"];
+                                    else
+                                        value += N[index]["cells"][index1]["value"][indexValue];
+
+
+                                }
+
+                                record.columns.Add(new Column(N[index]["cells"][index1]["columnId"], value));
+
+                                index1++;
+                            }
+                            grid.records.Add(record);
+
+                            //done 1 process check
+                            if (index == (step - 1))
+                            {
+                                doneOneProcess?.Invoke();
+                            }
+
+                            if (index == (step - 1) && autoDowload)
+                            {
+                                SetupRecords(grid, page + 1);
+                                return;
+                            }
+
+                            index++;
+                        }
+
+
+                        if (dowloadn == dowloadedTotal)
+                        {
+                            "Finished downloading".Print();
+                            doneOneProcess?.Invoke();
+                            finishAction?.Invoke();
+                            SaveProject();
+                        }
+
+                        //Done
+                        SetDirty();
+
+                      
+                       
+                        
+                    }
+                }
+            }
+
+
+            CancelWhenDone(action, unityWeb);
+
+
+        }
+
+
+        public void MergeRecord(Grid grid, int page)
+        {
+            if (grid == null)
+                return;
+            ("Setting up record for " + grid.nameGrid).Print();
+
+            UnityWebRequest unityWeb = UnityWebRequest.Get(GetRecordPage(grid.choesenViewID, page));
+            unityWeb.SetRequestHeader("Authorization", "ApiKey " + UserData.singleton.keyAPI);
+            unityWeb.SendWebRequest();
+
+            void action()
+            {
+                if (unityWeb.isDone)
+                {
+                    var i = unityWeb.downloadHandler.text;
+                    if (i.CheckOutput())
+                    {
+
+                        var N = JSON.Parse(i);
+                        int index = 0;
+                        //string _path = "";
+                        while (N[index].Count != 0)
+                        {
+
+                            Record record = new Record();
+
+                            record.recordID = N[index]["id"];
+                            record.pathTag = N[index]["path"];
+                            int index1 = 0;
+                            while (N[index]["cells"][index1].Count != 0)
+                            {
+                                string value = "";
+
+                                int lengthVal = N[index]["cells"][index1]["value"].Count;
+                                for (int indexValue = 0; indexValue <= lengthVal; indexValue++)
+                                {
+                                    if (value != "")
+                                        value += ";";
+                                    if (indexValue == 0)
+                                        value += N[index]["cells"][index1]["value"];
+                                    else
+                                        value += N[index]["cells"][index1]["value"][indexValue];
+
+
+                                }
+
+                                record.columns.Add(new Column(N[index]["cells"][index1]["columnId"], value));
+
+                                index1++;
+                            }
+
+                            Record _tempRecord = grid.records.Find(x => x.recordID == record.recordID);
+                            if (_tempRecord != null)
+                            {
+                                grid.records.Remove(_tempRecord);
+                            }
+                            grid.records.Add(record);
+                            index++;
+                        }
+
+
+                        //Project.singleton.Save();
+                    }
+                }
+
+
+
+            }
+            CancelWhenDone(action, unityWeb);
+
+        }
+        #endregion
+
+        public virtual void SetDirty()
+        {
+            
+        }
+
+        public virtual void SaveProject()
+        {
+
+        }
+
+        public static Action process;
+        public Action finishAction;
+        public Action doneOneProcess { get; set; }
+
+
+        public void CancelWhenDone(Action action, UnityWebRequest unityWebRequest)
+        {
+            CancelWhenDone(action, unityWebRequest, UserData.singleton.showServerMess);
+        }
+
+        public virtual void CancelWhenDone(Action action, UnityWebRequest unityWebRequest, bool printServerMes)
+        {
+            action += () =>
+            {
+                if (unityWebRequest.isDone)
+                {
+                    process -= action.Invoke;
+                    if (printServerMes)
+                        ("Server Message: " + unityWebRequest.downloadHandler.text).Print();
+                }
+            };
+
+            process += action.Invoke;
+        }
+
+
+
+
+    }
+
+}
