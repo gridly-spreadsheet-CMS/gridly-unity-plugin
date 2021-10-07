1136271ec4543c00d9dd054010093c7ab5c81716
vinpix <76024897+vinpix@users.noreply.github.com>
2021-10-07 09:05:21 +0700

    Add files via upload

---
+++ Gridly/Editor/Scripts/GridlyFunctionEditor.cs
@@ -0,0 +1,207 @@
+using UnityEngine.Networking;
+using UnityEditor;
+using System;
+using UnityEngine;
+using System.Text;
+using System.Collections.Generic;
+namespace Gridly.Internal
+{
+    public class GridlyFunctionEditor : GridlyFunction
+    {
+
+        public static GridlyFunctionEditor editor = new GridlyFunctionEditor();
+        #region UpdateDeleteAddRecord
+        public void DeleteRecord(string recordID, string viewID, Action success)
+        {
+            string a = "{\"ids\": [\"" + recordID + "\"]}";
+
+            byte[] data = Encoding.ASCII.GetBytes(a);
+
+            DownloadHandler downloadHandler = new DownloadHandlerBuffer();
+            UploadHandler uploadHandler = new UploadHandlerRaw(data);
+            UnityWebRequest unityWeb = new UnityWebRequest("https://api.gridly.com/v1/views/" + viewID + "/records", "DELETE", downloadHandler, uploadHandler);
+            unityWeb.SetRequestHeader("Content-Type", "application/json");
+            unityWeb.SetRequestHeader("Authorization", "ApiKey " + UserData.singleton.keyAPI);
+            unityWeb.SendWebRequest();
+            void action()
+            {
+                if (unityWeb.isDone)
+                {
+                    if (string.IsNullOrWhiteSpace(unityWeb.downloadHandler.text))
+                    {
+                        success?.Invoke();
+                    }
+                    else
+                    {
+                        var N = JSON.Parse(unityWeb.downloadHandler.text);
+                        if (N["code"] == "recordNotFound")
+                            success?.Invoke();
+
+                    }
+                }
+            }
+            CancelWhenDone(action, unityWeb); 
+        }
+        public void AddRecord(Record record, string viewID)
+        {
+            string a = "[{\"id\": \"" + record.recordID + "\",\"path\": \"" + record.pathTag + "\",\"cells\": [" + GetCode(record) + "]}]";
+            //a.Print();
+            AddUpdateRecord(a, viewID, true);
+        }
+
+
+        public void UpdateRecordLang(Record record, string viewID, Languages languages)
+        {
+            string a = "[{\"id\": \"" + record.recordID + "\",\"path\": \"" + record.pathTag + "\",\"cells\": [" + GetCodeLang(record, languages) + "]}]";
+            //a.Print();
+            AddUpdateRecord(a, viewID, false);
+        }
+
+
+        public void AddUpdateRecordAll(List<Record> records, string viewID, bool isAdd = true, bool isTargetLang = false)
+        {
+            const int limit = 1000;
+            int length = records.Count;
+            length /= limit;
+            for(int i =0; i < length+1; i++)
+            {
+                AddUpdateRecordAll(records, viewID, i * limit, (i + 1) * limit, isAdd, isTargetLang);
+            }
+
+        }
+        public void AddUpdateRecordAll(List<Record> records, string viewID, int from, int to, bool isAdd = true, bool isTargetLang = false)
+        {
+            
+            string a = "[";
+            int maxLength = records.Count;
+            //Debug.Log(from + " " + to + " " + maxLength);
+            for (int i =from; i<to; i++)
+            {
+                if (i >= maxLength)
+                    break;
+
+                Record record = records[i];
+
+                if (i > 0)
+                    a += ",";
+
+                if(!isTargetLang)
+                    a += "{\"id\": \"" + record.recordID + "\",\"path\": \"" + record.pathTag + "\",\"cells\": [" + GetCode(record) + "]}";
+                else
+                    a += "{\"id\": \"" + record.recordID + "\",\"path\": \"" + record.pathTag + "\",\"cells\": [" + GetCodeLang(record, UserData.singleton.mainLangEditor) + "]}";
+            }
+
+            a += "]";
+
+            //a.Print();
+            AddUpdateRecord(a, viewID, isAdd);
+        }
+
+
+        public void AddUpdateRecord(string a, string viewID ,bool isAdd = true)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(a);
+            DownloadHandler downloadHandler = new DownloadHandlerBuffer();
+            UploadHandler uploadHandler = new UploadHandlerRaw(data);
+
+            UnityWebRequest unityWeb;
+            if (isAdd)
+            {
+                unityWeb = new UnityWebRequest("https://api.gridly.com/v1/views/" + viewID + "/records", "POST", downloadHandler, uploadHandler);
+            }
+            else
+            {
+                unityWeb = new UnityWebRequest("https://api.gridly.com/v1/views/" + viewID + "/records", "PATCH", downloadHandler, uploadHandler);
+            }
+
+            unityWeb.SetRequestHeader("Content-Type", "application/json");
+            unityWeb.SetRequestHeader("Authorization", "ApiKey " + UserData.singleton.keyAPI);
+            unityWeb.SendWebRequest();
+
+
+            CancelWhenDone(() => { }, unityWeb);
+        }
+
+
+        static string GetCode(Record record)
+        {
+            string str = "";
+            int length = record.columns.Count;
+            for (int i = 0; i < length; i++)
+            {
+                if (i > 0)
+                    str += ",";
+                str += "{\"columnId\": \"" + record.columns[i].columnID + "\",\"value\": \"" + TextDataUploadHandle(record.columns[i].text, ref record) + "\"}";
+            }
+
+
+            return str;
+        }
+        static string GetCodeLang(Record record, Languages languages)
+        {
+            string str = "";
+            int length = record.columns.Count;
+            for (int i = 0; i < length; i++)
+            {
+                if (record.columns[i].columnID == languages.ToString())
+                {
+                    str += "{\"columnId\": \"" + record.columns[i].columnID + "\",\"value\": \"" + TextDataUploadHandle(record.columns[i].text, ref record) + "\"}";
+                    break;
+                }
+            }
+
+
+            return str;
+        }
+
+
+        public static string TextDataUploadHandle(string input, ref Record record)
+        {
+            string output = input;
+
+            if (string.IsNullOrEmpty(input))
+            {
+                foreach (var i in record.columns)
+                {
+                    if (i.columnID == UserData.singleton.mainLangEditor.ToString())
+                    {
+                        output = i.text;
+                        break;
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        #endregion
+
+        public override void CancelWhenDone(Action action, UnityWebRequest unityWebRequest, bool printServerMes)
+        {
+            action += () =>
+            {
+                if (unityWebRequest.isDone)
+                {
+                    EditorApplication.update -= action.Invoke;
+                    if (printServerMes)
+                        ("Server Message: " + unityWebRequest.downloadHandler.text).Print();
+                }
+            };
+
+            EditorApplication.update += action.Invoke;
+        }
+
+
+        public override void SaveProject()
+        {
+            Project.singleton.Save();
+        }
+        public override void SetDirty()
+        {
+            Project.singleton.setDirty();
+        }
+
+    }
+
+
+}
---
+++ Gridly/Editor/Scripts/TranslatorEditor.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Gridly;
+namespace Gridly.Internal
+{
+    
+    [CustomEditor(typeof(Translator))]
+    public class TranslatorEditor : Editor
+    {
+        GridlyArrData popupData = new GridlyArrData();
+
+        static string search = "";
+        Column chosenColum;
+        private void OnEnable()
+        {
+            search = "";
+        }
+        public override void OnInspectorGUI()
+        {
+            
+            base.OnInspectorGUI();
+            Translator translator = (Translator)target;
+
+            if (!popupData.init)
+            {
+                Refesh();
+                
+            }
+
+            if (Project.singleton.grids.Count == 0)
+                return;
+
+            //Db
+            GUILayout.Space(10);
+
+
+            //Grid
+            if (popupData.indexGrid == -1)
+                return;
+            GUILayout.Space(5);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Grid");
+            EditorGUI.BeginChangeCheck();
+            try
+            {
+                translator.grid = popupData.gridArr[EditorGUILayout.Popup(popupData.indexGrid, popupData.gridArr)];
+            }
+            catch
+            {
+                popupData.indexGrid = 0;
+            }
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(translator);
+                Refesh();
+            }
+            GUILayout.EndHorizontal();
+            
+
+            GUILayout.Space(10);
+
+
+            //Key
+
+
+            GUILayout.Space(5);
+            EditorGUI.BeginChangeCheck();
+            search = GUILayout.TextField(search, GUI.skin.GetStyle("ToolbarSeachTextField"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                
+                popupData.searchKey = search;
+                Refesh();
+            }
+
+            if (popupData.keyArr == null)
+                return;
+
+
+            GUILayout.Space(5);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Key");
+            EditorGUI.BeginChangeCheck();
+            try
+            {
+                translator.key = popupData.keyArr[EditorGUILayout.Popup(popupData.indexKey, popupData.keyArr)];
+            }
+            catch { GUILayout.Label("can't find key"); }
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(translator);
+                Refesh();
+
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(10);
+
+            try
+            {
+                Languages main = UserData.singleton.mainLangEditor;
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(main.ToString() + ": ");
+                if (chosenColum != null)
+                {
+                    chosenColum.text = GUILayout.TextArea(chosenColum.text);
+                    if(GUILayout.Button(new GUIContent() {text = "Export" , tooltip = "Export text to Girdly" }, GUILayout.MinWidth(60)))
+                    {
+                        GridlyFunctionEditor.editor.UpdateRecordLang(popupData.chosenRecord, popupData.grid.choesenViewID, UserData.singleton.mainLangEditor);
+                    }
+                }
+    
+                GUILayout.EndHorizontal();
+            }
+            catch { }
+
+
+        }
+
+        
+
+        void Refesh()
+        {
+            Translator translator = (Translator)target;
+            popupData.RefeshAll(translator.grid, translator.key);
+
+            try
+            {
+                Languages main = UserData.singleton.mainLangEditor;
+                chosenColum = popupData.chosenRecord.columns.Find(x => x.columnID == main.ToString());
+            }
+            catch { }
+            
+        }
+
+    }
+}
